8df8a475961ebe9ab9f6abe3397eea5c90a089fb
SarangPKarun <sarangspkach@gmail.com>
2024-10-16 17:22:23 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from './assets/sections/NavBar'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <NavBar />
+    </main>
+  )
+}
+
+export default App
